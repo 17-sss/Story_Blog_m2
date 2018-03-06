4cd0be877455b685db0ec665d2ce176e9fbe6cdf
xzxking <jhsonking@naver.com>
2018-03-06 18:21:54 +0900

    180306 PM 6:21 / 파일 업로드 (user_update) 시도..

--- src/com/db/DiaryDBBean.java
+++ src/com/db/DiaryDBBean.java
@@ -299,7 +299,37 @@ public class DiaryDBBean {
 	}
 	
 	// 일기 수정Pro 메소드 
-	public int updateDiary (DiaryDataBean diary) {
+		public int updateDiary (DiaryDataBean diary) {
+			String sql ="";
+			Connection conn = getConnection();
+			PreparedStatement pstmt = null;
+			int chk= 0; // int 변수 하나 생성.
+			ResultSet rs = null; 
+			
+			try {
+				conn = getConnection();
+				sql = "update diary set diaryid=?, subject=?, content=? where num=? and email = ?";
+				pstmt = conn.prepareStatement(sql);
+			
+				pstmt.setString(1, diary.getDiaryid());
+				pstmt.setString(2, diary.getSubject());
+				pstmt.setString(3, diary.getContent());
+				pstmt.setInt(4, diary.getNum());
+				pstmt.setString(5, diary.getEmail());
+				
+				chk = pstmt.executeUpdate(); //컬럼이 업데이트가 되었을때 숫자를 반환
+				pstmt.executeUpdate();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				close(conn, null, pstmt);
+			}
+			return chk;
+		}
+	
+	// 일기 수정Pro 메소드 - 파일 업로드 수정 시도
+	/*public int updateDiary (DiaryDataBean diary) {
 		String sql ="";
 		Connection conn = getConnection();
 		PreparedStatement pstmt = null;
@@ -308,14 +338,16 @@ public class DiaryDBBean {
 		
 		try {
 			conn = getConnection();
-			sql = "update diary set diaryid=?, subject=?, content=? where num=? and email = ?";
+			sql = "update diary set diaryid=?, subject=?, content=?, filename=?, filesize=? where num=? and email = ?";
 			pstmt = conn.prepareStatement(sql);
 		
 			pstmt.setString(1, diary.getDiaryid());
 			pstmt.setString(2, diary.getSubject());
 			pstmt.setString(3, diary.getContent());
-			pstmt.setInt(4, diary.getNum());
-			pstmt.setString(5, diary.getEmail());
+			pstmt.setString(4, diary.getFilename());
+			pstmt.setInt(5, diary.getFilesize());
+			pstmt.setInt(6, diary.getNum());
+			pstmt.setString(7, diary.getEmail());
 			
 			chk = pstmt.executeUpdate(); //컬럼이 업데이트가 되었을때 숫자를 반환
 			pstmt.executeUpdate();
@@ -327,7 +359,7 @@ public class DiaryDBBean {
 		}
 		return chk;
 		
-	}
+	}*/
 	
 	public int deleteDiary (int num, String email, String diaryid) throws Exception {
 		Connection conn = null;
--- src/controller/StoryController.java
+++ src/controller/StoryController.java
@@ -346,6 +346,70 @@ public class StoryController extends Action {
 			
 		return "/Project/view/user_updateDPro.jsp";
 	}
+	// 수정 - 파일 업로드 시도
+	/*public String user_updateDPro(HttpServletRequest req, HttpServletResponse res)  throws Throwable {
+		DiaryDataBean diary = new DiaryDataBean();
+		DiaryDBBean diaPro = DiaryDBBean.getInstance();
+		
+		// 6) fileSave 폴더 webcontent폴더 안에 만들기
+		String realFolder = ""; //웹 어플리케이션상의 절대경로
+		String encType = "euc-kr"; // 인코딩 타입
+		int maxSize = 5 *1024 * 1024; // 최대 업로드 될 파일 크기 .. 5MB
+		ServletContext context = req.getServletContext();
+		realFolder =context.getRealPath("fileSave");
+		MultipartRequest multi = null;
+		
+		// DefaultFileRenamePolicy는 중복된 파일 업로드할때 자동으로 Rename / aaa있으면 aaa(1)로
+		multi = new MultipartRequest(req, realFolder, maxSize, encType,  new DefaultFileRenamePolicy());
+		
+		Enumeration files = multi.getFileNames();
+		String filename="";
+		File file = null;
+		// =================================================
+		// 7) 
+		if (files.hasMoreElements()) { // 만약 파일이 다수면 if를 while로..
+			String name = (String) files.nextElement();
+			filename = multi.getFilesystemName(name); // DefaultFileRenamePolicy 적용
+			String original = multi.getOriginalFileName(name); // 파일 원래 이름 (추가해도되고, 안해도..?)
+			String type = multi.getContentType(name); // 파일 타입 (추가해도되고, 안해도..?)
+			file = multi.getFile(name);
+		}
+		
+		int num = Integer.parseInt(multi.getParameter("num"));
+		String pageNum = multi.getParameter("pageNum");
+		if (pageNum == null || pageNum == "") {pageNum = "1";}
+		String diaryid = multi.getParameter("diaryid");
+		if (diaryid==null) diaryid = "Main";
+		
+		try {
+			diary.setNum(num);
+			diary.setEmail(multi.getParameter("email"));
+			diary.setSubject(multi.getParameter("subject"));
+			diary.setContent(multi.getParameter("content"));
+			diary.setDiaryid(multi.getParameter("diaryid"));
+			diary.setIp(req.getRemoteAddr());
+			
+			if (file != null) {
+				diary.setFilename(filename);
+				diary.setFilesize((int)file.length());
+			} else {
+				diary.setFilename(" ");
+				diary.setFilesize(0);
+			}
+			
+			int chk = diaPro.updateDiary(diary);
+			
+			req.setAttribute("chk", chk);
+			req.setAttribute("pageNum", pageNum);
+			
+			System.out.println("수정여부: " + chk);
+			System.out.println(diary);
+			
+			
+		} catch (Exception e) {e.printStackTrace();}
+			
+		return "/Project/view/user_updateDPro.jsp";
+	}*/
 		
 		
 	public String user_deleteDPro(HttpServletRequest req, HttpServletResponse res)  throws Throwable { 
